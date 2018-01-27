f1a6787eb7613106df9fa44984410eb07c3d03c7
Alexandr Gavuka <alexandrgavuka@gmail.com>
2018-01-27 23:58:03 +0200

    added ssl

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-'use strict'
 var express = require('express');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var https = require('https'); 
+var fs = require('fs');
 
 var app = express();
 
-// app.get('/', function(req, res){
-//   res.send('hello world');
-// });
+var httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/example.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/example.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/example.com/chain.pem')
+};
 
+//for site
 app.use(express.static(__dirname + "./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+//for site
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+https.createServer(httpsOptions, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8000);
+
+
+
+
+
 
 app.listen(3000, err => {
     if (err) return console.log(err);
