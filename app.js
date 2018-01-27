eff74118a409e942e65dafbc7c49703a04de5a9b
Alexandr Gavuka <alexandrgavuka@gmail.com>
2018-01-27 17:05:55 +0200

    added index.html for genegate ssl

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+'use strict'
 var express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
+
+app.use(express.static(__dirname + "./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
 
 app.listen(3000, err => {
     if (err) return console.log(err);
